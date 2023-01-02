a601b5cd1ba94ff387743d3c22767f0cf21fea2c
tamago572 <110117095+tamago572@users.noreply.github.com>
2023-01-02 21:33:54 +0900

    PC版のスクリプトを追加

---
+++ content-desktop.js
@@ -0,0 +1,5 @@
+const pcPopup = document.getElementsByName('callout')[0];
+const mobilePopup = document.querySelector('mobile-promo');
+
+pcPopup.style.display = 'none';
+console.log('PC版ポップアップを非表示にしました');
