727e82fc960c973d2188b28839c4bceecb1d4598
tamago572 <110117095+tamago572@users.noreply.github.com>
2023-01-02 21:34:23 +0900

    コメントアウトを削除

--- content-script-mobile.js
+++ content-script-mobile.js
@@ -1,7 +1,5 @@
 const pcPopup = document.getElementsByName('callout')[0];
 const mobilePopup = document.querySelector('mobile-promo');
 
-// pcPopup.style.display = 'none';
-// console.log('PC版ポップアップを非表示にしました');
 mobilePopup.remove();
 console.log('スマホ版ポップアップを非表示にしました');
